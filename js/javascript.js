e62b305dea5423b094da806c1012ccc200f6dff5
Chris Ober <chrisober98@gmail.com>
2024-10-30 15:42:53 -0400

    Add a button that resets board to users input
    Added a button that accepts user input up to a max of 100 and resizes
    the board by doing so.  We accomplish this by clearing the board
    and remaking using the numbers the user enters.

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,58 @@
-console.log("Hey");
+let boardSize = 16;
 
 const container = document.querySelector("#container");
 
-for (let j = 0; j < 16; j++)
-{
-const newFlex = document.createElement("div");
-newFlex.style.display = "flex";
+// Function to create the initial board
+function createBoard(size) {
+    // Clear the existing board
+    container.innerHTML = ''; // This will remove all child elements
 
-    for (let i = 0; i < 16; i++)
-    {
-        const newDiv = document.createElement("div");
+    // Loop through rows
+    for (let i = 0; i < size; i++) {
+        const newFlex = document.createElement("div");
+        newFlex.style.display = "flex";
 
-        newDiv.style.borderColor = "red";
-        newDiv.style.borderWidth = "1px";
-        newDiv.style.borderStyle = "solid";
-        newDiv.style.width = "100px";
-        newDiv.style.height = "100px";
+        // Loop through columns
+        for (let j = 0; j < size; j++) {
+            const newDiv = document.createElement("div");
 
-        newFlex.append(newDiv);   
-    }      
-container.append(newFlex);
+            newDiv.style.borderColor = "red";
+            newDiv.style.borderWidth = "1px";
+            newDiv.style.borderStyle = "solid";
+            newDiv.style.width = "100px";
+            newDiv.style.height = "100px";
+
+            newDiv.addEventListener('mouseenter', () => {
+                newDiv.style.backgroundColor = 'lightcoral'; 
+                newDiv.style.borderColor = 'darkred';   
+            });
+
+            newFlex.append(newDiv);
+        }
+
+        container.append(newFlex);
+    }
 }
+
+// Create the initial board
+createBoard(boardSize);
+
+// Function to reset the board with user input size
+function resetBoard() {
+    let inputSize = prompt("Size (max 100): ");
+
+    // Convert the input size to a number
+    inputSize = Number(inputSize);
+
+    if (inputSize > 100 || isNaN(inputSize) || inputSize <= 0) {
+        alert("SIZE TOO BIG DUDE! Please enter a number between 1 and 100.");
+        return; // Exit the function if the size is invalid
+    }
+
+    // Create a new board with the specified size
+    createBoard(inputSize);
+}
+
+// Get the button and set up the event listener
+const button = document.getElementById("inputButton"); // Change selector to use ID without dot
+button.addEventListener('click', resetBoard);
